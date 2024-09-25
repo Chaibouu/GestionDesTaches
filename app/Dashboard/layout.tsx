a217c1b7e3b02fe5372df7ddf978828cbb299d82
chaibouu <chaibouabdoulwahab@gmail.com>
2024-09-25 19:43:01 +0200

    add dashboradNav

---
+++ app/Dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export default async function DasboardLayout({children}: Readonly<{children: React.ReactNode}>){
+    return (
+        <section className="max-w-[1200px] mx-auto md-flex md:items-center md:gap-4 h-screen w-full mt-2 p-2">
+
+            <div className="w-full h-full">
+                {children}
+            </div>
+
+        </section>
+    )
+}
---
+++ components/DashboardNav/DashboardNav.tsx
@@ -0,0 +1,21 @@
+"use client"
+import {Notebook, Settings, CreditCard} from 'lucide-react'
+import Link from "next/link"
+import {usePathname} from "next/navigation"
+
+export default function DashboardNav(){
+    const pathname = usePathname();
+
+    const menuDashboard = [
+        {name:"Tâches", icon: Notebook, path:"/Dashoard/taches"},
+        {name:"Settings", icon: Settings, path:"/Dashoard/settings"},
+        // {name:"Tâches", icon: Notebook, path:"/Dashoard/taches"},
+    ]
+    return(
+        <nav className="flex md:flex-col md:h-full md:w-16 w-full lg:w-40 gap-2">
+            {menuDashboard.map((link, index)=>{
+                const isActive = pathname.starWith(link.path)
+            })}
+        </nav>
+    )
+}
