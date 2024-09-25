da6069c7e391af5ac53701aad04bd7db3e3795b8
chaibouu <chaibouabdoulwahab@gmail.com>
2024-09-25 19:55:36 +0200

    add dashboradNav

--- components/DashboardNav/DashboardNav.tsx
+++ components/DashboardNav/DashboardNav.tsx
@@ -17,8 +17,9 @@ export default function DashboardNav(){
                 const isActive = pathname.starWith(link.path)
                 return(
                     <Link href={link.path}>
-                        <div className="flex items-center ">
-
+                        <div className={`flex items-center justify-center lg:justify-start gap-2 cursor-pointer lg:p-3 p-2 hover:bg-green-500 hover:bg-opacity-50 hover:text-white text-sm font-bold rounded-md ${isActive && "bg-green-500 text-white"}`}>
+                            <link.icon className="w-4"/>
+                            <span className="hidden lg:block">{link.name}</span>
                         </div>
                     </Link>
                 )
