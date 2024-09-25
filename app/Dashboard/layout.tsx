e2420b594c21325b2586b2473a2a9f0bafa95655
chaibouu <chaibouabdoulwahab@gmail.com>
2024-09-25 20:38:10 +0200

    add dashboradNav

--- app/Dashboard/layout.tsx
+++ app/Dashboard/layout.tsx
@@ -1,7 +1,9 @@
+import DashboardNav from "@/components/DashboardNav/DashboardNav"
 export default async function DasboardLayout({children}: Readonly<{children: React.ReactNode}>){
     return (
         <section className="max-w-[1200px] mx-auto md-flex md:items-center md:gap-4 h-screen w-full mt-2 p-2">
 
+            <DashboardNav />
             <div className="w-full h-full">
                 {children}
             </div>
--- components/DashboardNav/DashboardNav.tsx
+++ components/DashboardNav/DashboardNav.tsx
@@ -7,14 +7,14 @@ export default function DashboardNav(){
     const pathname = usePathname();
 
     const menuDashboard = [
-        {name:"Tâches", icon: Notebook, path:"/Dashoard/taches"},
-        {name:"Settings", icon: Settings, path:"/Dashoard/settings"},
-        {name:"Tâches", icon: CreditCard, path:"/Dashoard/taches"},
+        {name:"Tâches", icon: Notebook, path:"/Dashboard/taches"},
+        {name:"Settings", icon: Settings, path:"/Dashboard/settings"},
+        {name:"payements", icon: CreditCard, path:"/Dashboard/taches"},
     ]
     return(
         <nav className="flex md:flex-col md:h-full md:w-16 w-full lg:w-40 gap-2">
             {menuDashboard.map((link, index)=>{
-                const isActive = pathname.starWith(link.path)
+                const isActive = pathname.startsWith(link.path)
                 return(
                     <Link href={link.path}>
                         <div className={`flex items-center justify-center lg:justify-start gap-2 cursor-pointer lg:p-3 p-2 hover:bg-green-500 hover:bg-opacity-50 hover:text-white text-sm font-bold rounded-md ${isActive && "bg-green-500 text-white"}`}>
