0e2fafa442a861065a56931f77bd50ba48adeac1
chaibouu <chaibouabdoulwahab@gmail.com>
2024-09-25 20:50:17 +0200

    add Taches

--- app/Dashboard/taches/page.tsx
+++ app/Dashboard/taches/page.tsx
@@ -1,4 +1,5 @@
-
+import Link from "next/link"
+import {Button} from "@/components/ui/button"
 
 export default function page() {
   return (
@@ -7,8 +8,11 @@ export default function page() {
         <div className="grid gap-1">
             <h2 className="text-3xl uppercase font-black">Tâches</h2>
             <p className=" text-lg text-muted-foreground">Ne perdez pas vos idée, prennez des notes</p>
-            
+            <div className="w-12 bg-white my-2 mx-1 h-[1px]"></div>
         </div>
+        <Button>
+            <Link href="/"> Ajouer une Tâche</Link>
+        </Button>
       </div>
     </section>
   )
