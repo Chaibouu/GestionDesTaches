710a612ca827a40ca560847223bf62a6d0a0fc38
chaibouu <chaibouabdoulwahab@gmail.com>
2024-09-25 20:46:00 +0200

    add Taches

--- app/Dashboard/taches/page.tsx
+++ app/Dashboard/taches/page.tsx
@@ -2,10 +2,14 @@
 
 export default function page() {
   return (
-    <>
-      <div>
-        Tâches
+    <section className="grid items-start gap-y-8">
+      <div className=" flex md:items-center md:justify-between flex-col md:flex-row px-2">
+        <div className="grid gap-1">
+            <h2 className="text-3xl uppercase font-black">Tâches</h2>
+            <p className=" text-lg text-muted-foreground">Ne perdez pas vos idée, prennez des notes</p>
+            
+        </div>
       </div>
-    </>
+    </section>
   )
 }
