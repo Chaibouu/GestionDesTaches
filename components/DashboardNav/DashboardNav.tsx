b6bdd23b0cc38cae7679cc333622aa3416a100d8
chaibouu <chaibouabdoulwahab@gmail.com>
2024-09-25 19:46:42 +0200

    add dashboradNav

--- components/DashboardNav/DashboardNav.tsx
+++ components/DashboardNav/DashboardNav.tsx
@@ -9,12 +9,19 @@ export default function DashboardNav(){
     const menuDashboard = [
         {name:"Tâches", icon: Notebook, path:"/Dashoard/taches"},
         {name:"Settings", icon: Settings, path:"/Dashoard/settings"},
-        // {name:"Tâches", icon: Notebook, path:"/Dashoard/taches"},
+        {name:"Tâches", icon: CreditCard, path:"/Dashoard/taches"},
     ]
     return(
         <nav className="flex md:flex-col md:h-full md:w-16 w-full lg:w-40 gap-2">
             {menuDashboard.map((link, index)=>{
                 const isActive = pathname.starWith(link.path)
+                return(
+                    <Link href={link.path}>
+                        <div className="flex items-center ">
+
+                        </div>
+                    </Link>
+                )
             })}
         </nav>
     )
